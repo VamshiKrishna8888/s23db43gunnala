eb533b9ce17e2d04e1775cce3b23cb73e7c68db3
VamshiKrishna8888 <137825940+VamshiKrishna8888@users.noreply.github.com>
2023-11-28 15:36:59 -0600

    made changes to local repo

--- models/music.js
+++ models/music.js
@@ -1,7 +1,18 @@
 const mongoose = require("mongoose")
 const musicSchema = mongoose.Schema({
-name: String,
-type: String,
-No_of_songs : Number
-})
+  name: {
+    type: String,
+    required: true,
+  },
+ type: {
+    type: String,
+    required: true,
+  },
+  No_of_songs: {
+    type: Number,
+    required: true,
+    min: [0, 'Price must be at least 0'],   // Minimum value for the price
+    max: [300, 'Price must be at most 100'], // Maximum value for the price
+  },
+});
 module.exports = mongoose.model("music",musicSchema)
--- routes/music.js
+++ routes/music.js
@@ -1,6 +1,15 @@
 var express = require('express');
 const music_controlers = require('../controllers/music');
 var router = express.Router();
+// A little function to check if we have an authorized user and continue on
+// redirect to login.
+const secured = (req, res, next) => {
+ if (req.user){
+ return next();
+ }
+ res.redirect("/login");
+ }
+
 /* GET home page. */
 router.get('/' , music_controlers.music_view_all_Page)
 
@@ -19,7 +28,7 @@ router.get('/detail', music_controlers.music_view_one_Page);
 router.get('/create', music_controlers.music_create_Page);
 
 /* GET create update page */
-router.get('/update', music_controlers.music_update_Page);
+router.get('/update',secured, music_controlers.music_update_Page);
 
 /* GET delete music page */
 router.get('/delete', music_controlers.music_delete_Page);
